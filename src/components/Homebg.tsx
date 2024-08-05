9a4bf318e23e8d71ac3102a5fbf721b3986d68b5
RWIGARA Rodrigue <rodrirwigara@gmail.com>
2024-08-05 09:54:02 +0200

    gg

--- src/components/Homebg.tsx
+++ src/components/Homebg.tsx
@@ -3,15 +3,15 @@ import React from 'react'
 
 const Homebg:React.FC = () => {
   return (
-    <div className="bg-gray-300 m-0 w-full h-full absolute overflow-hidden">
+    <div className="bg-gray-300 m-0 w-full h-full  flex flex-row justify-between  absolute overflow-hidden">
       <div className="clip-custom-3 w-7/12 h-1/4  bg-gradient-to-t from-cyan-400 to-cyan-600 " />
-      {/* <div className="" style={{ backgroundImage: "url('/b.jpg')" }}>
+      <div className=" bg-[url('/1.png')] w-1/2 h-full mr-2 hidden lg:block  p-2 ">
         <img
-          src="/1.jpg"
-          alt="edge"
-          className="clip-custom-1 clip-custom-2 lg:h-5/6 lg:w-1/3 sm:w-2/6  w-0  lg:border-l-8 border-black   fixed right-0 top-0 "
+          src="/edge.jpg"
+          className="hidden lg:block  rounded-bl-full h-full "
+          alt=""
         />
-      </div> */}
+      </div>
     </div>
   );
 }
