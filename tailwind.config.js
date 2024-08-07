cdb5363583e5c241147b606026facd127de21c7e
RWIGARA Rodrigue <rodrirwigara@gmail.com>
2024-08-07 08:23:10 +0200

    correct hero page

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,20 +13,35 @@ export default {
       backgroundImage: {
         'hero-pattern': "url('/images/background.jpg')",
       },
+    keyframes: {
+      slide: {
+        '0%': { transform: 'translateX(0%)' },
+        '20%': { transform: 'translateX(-100%)' },
+        '40%': { transform: 'translateX(-200%)' },
+        '60%': { transform: 'translateX(-300%)' },
+        '80%': { transform: 'translateX(-400%)' },
+        '100%': { transform: 'translateX(-500%)' },
+      },
     },
+    animation: {
+      slide: 'slide 10s linear infinite',
+    },
+  }
+    ,
+  
   plugins: [
-    function ({ addUtilities }) {
-      const newUtilities = {
-        '.clip-custom-3': {
-          clipPath: 'polygon(0% -4%, 60% 1%, 61% 34%, -1% 100%)',
-        },
-        '.clip-custom-1': {
-          clipPath: 'polygon(100% 0%, 75% 50%, 100% 100%, 25% 100%, 0% 50%, 25% 0%)'
-        }
+    // function ({ addUtilities }) {
+    //   const newUtilities = {
+    //     '.clip-custom-3': {
+    //       clipPath: 'polygon(0% -4%, 60% 1%, 61% 34%, -1% 100%)',
+    //     },
+    //     '.clip-custom-1': {
+    //       clipPath: 'polygon(100% 0%, 75% 50%, 100% 100%, 25% 100%, 0% 50%, 25% 0%)'
+    //     }
 
-      }
+    //   }
 
-      addUtilities(newUtilities, ['responsive', 'hover']);
-    },
+    //   addUtilities(newUtilities, ['responsive', 'hover']);
+    // },
   ],
 }
