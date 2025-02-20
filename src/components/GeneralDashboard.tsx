6df7a2650e1b2772a4b5551a94da499b3a15fb01
RWIGARA Rodrigue <rodrirwigara@gmail.com>
2025-02-20 19:05:30 +0200

    loading state

--- src/components/GeneralDashboard.tsx
+++ src/components/GeneralDashboard.tsx
@@ -6,7 +6,7 @@ interface DashboardProps {
   MainComponent: React.ComponentType; // Accept a component as a prop
 }
 const Dashboard = ({ MainComponent }: DashboardProps) => {
-  const { getUser, loggedUser} = useAuth();
+  const { getUser, loggedUser,isLoading} = useAuth();
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -20,7 +20,7 @@ const Dashboard = ({ MainComponent }: DashboardProps) => {
 
 
   
-  if (!loggedUser) return <div className="flex items-center justify-center h-screen">No user found</div>;
+  if (isLoading) return <div className="flex items-center justify-center h-screen">loading...</div>;
 
   return (
     <div className="h-screen flex flex-col">
